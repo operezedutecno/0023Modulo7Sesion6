cb5da3cad22370ad9e4185e465cb1cebef68db7a
Osman Pérez <operez@edutecno.com>
2023-07-13 21:09:35 -0400

    Obtener datos de registro al aplicar INSERT INTO

--- index.js
+++ index.js
@@ -39,4 +39,30 @@ const actualizacionPrecios = async () => {
     }
 }
 
-actualizacionPrecios()
+// actualizacionPrecios()
+
+const procesoVenta = async() => {
+    const client = await pool.connect()
+    try {
+        await client.query("BEGIN") //Iniciar la transacción
+        const consultaCliente = {
+            text: "INSERT INTO clientes(cl_rut, cl_dv, cl_nombre, cl_apellido, cl_direccion, cl_telefono, cl_correo,cl_fecha_nacimiento) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
+            values: [44444444, 4, "Luis", "Medina", "Santiago", "+56912345678","lmedina@mail.com","1980-05-15"]
+        }
+        const registroCliente = await client.query(consultaCliente);
+
+        const idCliente = registroCliente.rows[0].cl_id
+
+
+        // Ejecutar la consulta de registro de colaborador
+
+        // Ejecutar consulta de registro de venta
+        
+        console.log(idCliente);
+    } catch (error) {
+        console.log(error.message);
+    } finally {
+        client.end()
+    }
+}
+procesoVenta()

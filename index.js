0d3476950afb82d60a6946635dd8e1c9750c3865
Osman Pérez <operez@edutecno.com>
2023-07-14 20:34:49 -0400

    Avances proceso de venta

--- index.js
+++ index.js
@@ -45,22 +45,64 @@ const procesoVenta = async() => {
     const client = await pool.connect()
     try {
         await client.query("BEGIN") //Iniciar la transacción
+
+        //Registro de Cliente
         const consultaCliente = {
             text: "INSERT INTO clientes(cl_rut, cl_dv, cl_nombre, cl_apellido, cl_direccion, cl_telefono, cl_correo,cl_fecha_nacimiento) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *",
             values: [44444444, 4, "Luis", "Medina", "Santiago", "+56912345678","lmedina@mail.com","1980-05-15"]
         }
         const registroCliente = await client.query(consultaCliente);
-
         const idCliente = registroCliente.rows[0].cl_id
+        console.log("idCliente", idCliente);
+        //Fin Registro de Cliente
 
 
-        // Ejecutar la consulta de registro de colaborador
+        //Registro de Colaborador
+        const consultaColaborador = {
+            text: "INSERT INTO colaboradores(col_rut, col_dv, col_nombre, col_apellido) VALUES($1, $2, $3, $4) RETURNING *",
+            values: [5243077, 1, "María", "León"]
+        }
+        const registroColaborador = await client.query(consultaColaborador)
+        const idColaborador = registroColaborador.rows[0].col_id
+        console.log("idColaborador",idColaborador)
+        // Fin Registro de Colaborador
 
-        // Ejecutar consulta de registro de venta
+
+        //Registro de Productos
+        const consultaProducto1 = {
+            text: "INSERT INTO productos(prod_codigo, prod_nombre, prod_precio, prod_stock) VALUES($1, $2, $3, $4) RETURNING *",
+            values: ["MART001","Martillo", 25000, 100]
+        }
+        const registroProducto1 = await client.query(consultaProducto1)
+        const idProducto1 = registroProducto1.rows[0].prod_id
+        console.log("idProducto1", idProducto1);
+
+
+        const consultaProducto2 = {
+            text: "INSERT INTO productos(prod_codigo, prod_nombre, prod_precio, prod_stock) VALUES($1, $2, $3, $4) RETURNING *",
+            values:["DEST001", "Destornillador de cruz", 15000, 250]
+        }
+        const registroProducto2 = await client.query(consultaProducto2)
+        const idProducto2 = registroProducto2.rows[0].prod_id
+        console.log("idProducto2", idProducto2);
+        // Fin Registro de Productos
+
+
+
+        // Registro de venta
+        const consultaVenta = {
+            text: "INSERT INTO ventas(ven_id_cliente, ven_id_colaborador, ven_id_tipo_comprobante, ven_fecha_hora, ven_iva, ven_tipo_pago) VALUES($1, $2, $3, NOW(), $4, $5) RETURNING *",
+            values: [idCliente, idColaborador, 1, 19,"Efectivo"]
+        }
+        const registroVenta = await client.query(consultaVenta)
+        const idVenta = registroVenta.rows[0].ven_numero_transaccion
+        console.log("idVenta", idVenta);
+        // Fin Registro de venta
         
-        console.log(idCliente);
     } catch (error) {
         console.log(error.message);
+        // Finalización de la transacción en caso de error.
+        await client.query("ROLLBACK")
     } finally {
         client.end()
     }
